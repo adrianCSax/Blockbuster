8f8a4db80ddd8eec3c6b733cc290249cfb399c4e
AdrianClementSax <adrianclement1234@icloud.com>
2021-10-21 17:07:01 +0200

    inicio.php added

--- 222.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "Dvd.php";
-
-$miDvd = new Dvd("Origen", 24, 15, "es,en,fr", "16:9"); 
-echo "<strong>" . $miDvd->titulo . "</strong>"; 
-echo "<br>Precio: " . $miDvd->getPrecio() . " euros"; 
-echo "<br>Precio IVA incluido: " . $miDvd->getPrecioConIva() . " euros";
-$miDvd->mostrarResumen();
---
+++ inicio.php
@@ -0,0 +1,36 @@
+<?php
+include "Soporte.php";
+include "CintaVideo.php";
+include "Dvd.php";
+include "Juego.php";
+
+// 220
+$soporte1 = new Soporte("Tenet", 22, 3); 
+echo "<strong>" . $soporte1->titulo . "</strong>"; 
+echo "<br>Precio: " . $soporte1->getPrecio() . " euros"; 
+echo "<br>Precio IVA incluido: " . $soporte1->getPrecioConIVA() . " euros";
+$soporte1->mostrarResumen();
+
+// 221
+echo "<hr>";
+$miCinta = new CintaVideo("Los cazafantasmas", 23, 3.5, 107); 
+echo "<strong>" . $miCinta->titulo . "</strong>"; 
+echo "<br>Precio: " . $miCinta->getPrecio() . " euros"; 
+echo "<br>Precio IVA incluido: " . $miCinta->getPrecioConIva() . " euros";
+$miCinta->mostrarResumen();
+
+// 222
+echo "<hr>";
+$miDvd = new Dvd("Origen", 24, 15, "es,en,fr", "16:9"); 
+echo "<strong>" . $miDvd->titulo . "</strong>"; 
+echo "<br>Precio: " . $miDvd->getPrecio() . " euros"; 
+echo "<br>Precio IVA incluido: " . $miDvd->getPrecioConIva() . " euros";
+$miDvd->mostrarResumen();
+
+// 223
+echo "<hr>";
+$miJuego = new Juego("The Last of Us Part II", 26, 49.99, "PS4", 1, 1); 
+echo "<strong>" . $miJuego->titulo . "</strong>"; 
+echo "<br>Precio: " . $miJuego->getPrecio() . " euros"; 
+echo "<br>Precio IVA incluido: " . $miJuego->getPrecioConIva() . " euros";
+$miJuego->mostrarResumen();
