b0a7477610417391bfa1b88fdb781f0265f07156
Damian Martin <damianmd251@gmail.com>
2021-10-22 16:10:59 +0200

    funcion devolver Cliente

--- Cliente.php
+++ Cliente.php
@@ -1,6 +1,7 @@
 <?php
 
-class Cliente {
+class Cliente
+{
 
     private array $soportesAlquilados;
     private int $numSoprtesAlquilados;
@@ -9,33 +10,53 @@ class Cliente {
         public string $nombre,
         private string $numero,
         private int $maxAlquilerConcurrente = 3
-    ){
+    ) {
         $this->soportesAlquilados = [];
     }
 
-    public function getSoportesAlquilados() : array {
+    public function getSoportesAlquilados(): array
+    {
         return $this->soportesAlquilados;
     }
 
-    public function getNumero() : int {
+    public function getNumero(): int
+    {
         return $this->numero;
     }
-    public function setNumero(int $numero) : void {
+    public function setNumero(int $numero): void
+    {
         $this->numero = $numero;
     }
 
-    public function tieneAlquilado(Soporte $soporte) : bool {
+    public function tieneAlquilado(Soporte $soporte): bool
+    {
         return in_array($soporte, $this->soportesAlquilados);
     }
 
-    public function alquilar(Soporte $soporte) : bool {
+    public function alquilar(Soporte $soporte): bool
+    {
         if (!$this->tieneAlquilado($soporte) && ($this->numSoprtesAlquilados < $this->maxAlquilerConcurrente)) {
             array_push($this->soportesAlquilados, $soporte);
             $this->numSoprtesAlquilados++;
-            echo "<strong>Alquilado soporte a </strong>" . $this->nombre; 
+            echo "<strong>Alquilado soporte a </strong>" . $this->nombre;
+            $soporte->mostrarResumen();
+
             return true;
-        } 
-        echo "<strong>No se ha podido alquilar</strong>" . $this->nombre; 
+        }
+        echo "<strong>No se ha podido alquilar</strong>" . $this->nombre;
+        return false;
+    }
+
+    public function devolver(int $numSoporte): bool
+    {
+        foreach ($this->soportesAlquilados as $soporte) {
+            if ($soporte->getNumero() == $numSoporte) {
+                if (($key = array_search($soporte, $this->soportesAlquilados)) !== FALSE) {
+                    unset($this->soportesAlquilados[$key]);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
